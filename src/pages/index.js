30199ea6614ec1359f87dab8b7c1db7f0374562c
Camila Zucchi <camilazucchi@outlook.com>
2023-05-02 16:25:51 -0300

    feat: add carousel component to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import Banner from "../components/banner/index.jsx";
+import Footer from "../components/footer/index.jsx";
 import Header from "../components/header/index.jsx";
+import Carousel from "../components/carousel/index.jsx";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Banner />
+      <Carousel />
+      <Footer />
     </>
   );
 }
